5a9c8e637cb168f2484e86f5db7a799bcfd00d7e
Hanzi448 <153763184+Hanzi448@users.noreply.github.com>
2025-12-25 17:10:26 +0500

    Update

---
+++ app/models/customer.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.database import Base
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    customer_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    contact_no = Column(String(20), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    address = Column(Text)
+    gender = Column(String(10))
+    nationality = Column(String(50))
+    
---
+++ app/models/destination.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text, Numeric, CheckConstraint
+from app.database import Base
+
+class Destination(Base):
+    __tablename__ = "destinations"
+
+    destination_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    country = Column(String(100), nullable=False)
+    description = Column(Text)
+    average_cost = Column(Numeric(10, 2), CheckConstraint('average_cost >= 0'))
